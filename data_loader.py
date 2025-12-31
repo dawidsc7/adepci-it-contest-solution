8623e5c04393223093289121f8019eefa8004991
Dawid <dawidksciurka@gmail.com>
2025-12-31 21:11:38 +0100

    Add CLI support for flexible input (file/direct text) for encrypted message instead of hard-coded text

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,19 @@
-def get_encrypted_text():
+import os
+def get_encrypted_text(source=None):
     """
-    Returns the encrypted text (ciphertext) to be decrypted.
+    Reads encrypted text from user-given file or returns provided string.
+    
+    Args:
+        source(str, optional): Path to file or direct ciphertext.
+        If None, uses default 'ciphertext.txt'
     
     Returns:
         str: The encrypted message using Caesar cipher.
     """
-    return "epomj ezno yudndve. nuopxuiv diozgdbzixev rkgtrv iv ivnuv xjyudziijnx. vwt fjiotipjrvx rturvidz, rtngde fjy uvyvidv iv: epomj.ezno.yudndve@vyzkxd.do"
+    if source is None:
+       source = "ciphertext.txt"
+    if os.path.isfile(source):
+        with open(source, "r", encoding="utf-8") as file:
+            return file.read().strip()
+    else:
+        return source
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from data_loader import get_encrypted_text
 from cipher_breaker import find_solution
 from results import save_results
-
+import sys
 
 def main():
     """
     Main entry point for the Caesar cipher breaker program.
-    
+
+    Usage:
+        python main.py                # Uses default ciphertext.txt
+        python main.py myfile.txt     # Uses custom file
+        python main.py "text..."      # Uses direct text
+
     1. Loads the encrypted text
     2. Finds the correct decryption using brute-force + language detection
     3. Saves and displays the results
     """
-    ciphertext = get_encrypted_text()
+    
+    source = sys.argv[1] if len(sys.argv) > 1 else None
+    ciphertext = get_encrypted_text(source)
     best_text, correct_shift, all_versions = find_solution(ciphertext)
     save_results(best_text, correct_shift, all_versions)
 
